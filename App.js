6b4f87a746e5216261f9e5798a25bbf8651bda46
Ribhu Ghosh <102036665+Ribhu-7@users.noreply.github.com>
2022-07-27 19:59:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 const express = require ("express")
 const app = express()
 const DataRouter = require("./Routes/data")
+const cors = require("cors");
 
 // app.get("/home", function (req,res) {
 
 //     res.send("Home")
 // })
+app.use(cors());
 
 app.use("/api",DataRouter)
 
